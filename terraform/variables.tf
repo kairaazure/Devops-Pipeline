4cb70d2b5103aace13ad1395c6b8b1b3ef313887
Nikhil Gupta <nikgupta5@publicisgroupe.net>
2020-02-03 22:08:32 +0530

    terraform variables and buildspec

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -9,3 +9,23 @@ default = "192.168.1.0/24"
 variable "availabilityZone" {
 default = "us-east-1a"
 }
+
+variable "mapPublicIP" {
+default = true 
+}
+
+variable "SG_CIDRblock" {
+default = "122.162.206.189/32"
+}
+
+variable "destinationCIDRblock" {
+default = "122.162.206.189/32"
+}
+
+variable "internetip" {
+default = "0.0.0.0./0"
+}
+
+variable "region" {
+default = "us-east-1"
+}
