a2e1974865ea570b2f8ef6b497b83f3c18a0cb43
Nikhil Gupta <nikgupta5@publicisgroupe.net>
2020-02-04 08:59:19 +0530

    terraform variables and buildspec

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,12 +1,12 @@
 provider "aws" {
   version = "~> 2.0"
-  region = var.region
+  region = "${var.region}"
 }
 
 
 #Create VPC
 resource "aws_vpc" "terraform_vpc" {
-  cidr_block = "var.VpcCidrBlock"
+  cidr_block = "${var.VpcCidrBlock}"
   instance_tenancy = "default"
   
 }
@@ -23,9 +23,9 @@ resource "aws_subnet" "terraform_subnet_public"
 {
 
 vpc_id = aws_vpc.terraform_vpc.id
-cidr_block = var.SubnetCidrBlock
-availability_zone = var.availabilityZone
-map_public_ip_on_launch = var.mapPublicIP
+cidr_block = "${var.SubnetCidrBlock}"
+availability_zone = "${var.availabilityZone}"
+map_public_ip_on_launch = "${var.mapPublicIP}"
 
 tags {
 
@@ -44,7 +44,7 @@ resource "aws_security_group" "terraform_SG1"
   
   ingress {
   
-    cidr_blocks = var.SG_CIDRblock
+    cidr_blocks = "${var.SG_CIDRblock}"
     from_port = 22
     to_port = 22
     protocol = "tcp"	
@@ -77,7 +77,7 @@ resource "aws_network_acl" "terraform_NACL" {
     protocol   = "tcp"
     rule_no    = 100
     action     = "allow"
-    cidr_block = var.destinationCIDRblock 
+    cidr_block = "${var.destinationCIDRblock}" 
     from_port  = 22
     to_port    = 22
   }
@@ -87,7 +87,7 @@ resource "aws_network_acl" "terraform_NACL" {
     protocol   = "tcp"
     rule_no    = 200
     action     = "allow"
-    cidr_block = var.destinationCIDRblock 
+    cidr_block = "${var.destinationCIDRblock}" 
     from_port  = 80
     to_port    = 80
   }
@@ -97,7 +97,7 @@ resource "aws_network_acl" "terraform_NACL" {
     protocol   = "tcp"
     rule_no    = 300
     action     = "allow"
-    cidr_block = var.destinationCIDRblock
+    cidr_block = "${var.destinationCIDRblock}"
     from_port  = 1024
     to_port    = 65535
   }
@@ -107,7 +107,7 @@ resource "aws_network_acl" "terraform_NACL" {
     protocol   = "tcp"
     rule_no    = 100
     action     = "allow"
-    cidr_block = var.destinationCIDRblock
+    cidr_block = "${var.destinationCIDRblock}"
     from_port  = 22 
     to_port    = 22
   }
@@ -117,7 +117,7 @@ resource "aws_network_acl" "terraform_NACL" {
     protocol   = "tcp"
     rule_no    = 200
     action     = "allow"
-    cidr_block = var.destinationCIDRblock
+    cidr_block = "${var.destinationCIDRblock}"
     from_port  = 80  
     to_port    = 80 
   }
@@ -127,7 +127,7 @@ resource "aws_network_acl" "terraform_NACL" {
     protocol   = "tcp"
     rule_no    = 300
     action     = "allow"
-    cidr_block = var.destinationCIDRblock
+    cidr_block = "${var.destinationCIDRblock}"
     from_port  = 1024
     to_port    = 65535
   }
@@ -162,7 +162,7 @@ resource "aws_route_table" "terraform_VPC_route_table" {
 
 resource "aws_route" "terraform_VPC_internet_access" {
   route_table_id         = aws_route_table.terraform_VPC_route_table.id
-  destination_cidr_block = var.internetip
+  destination_cidr_block = "${var.internetip}"
   gateway_id             = aws_internet_gateway.terraform_VPC_GW.id
 } # end resource
 
