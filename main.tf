fee1860ebd2e1beb287bad12a53ba9aeb5c87208
Nikhil Gupta <nikgupta5@publicisgroupe.net>
2020-02-04 20:28:18 +0530

    added module for vpc and ec2

--- main.tf
+++ main.tf
@@ -9,14 +9,10 @@ provider "aws" {
 
 module "vpc" {
   source = "./modules/terraform-vpc"
-  version                = "~> 2.0"
-
-
 }
 
 module "ec2" {
   source                 = "./modules/terraform-ec2-instance"
-  version                = "~> 2.0"
   vpc_security_group_ids = module.vpc.vpc_SG1
   subnet_id              = module.vpc.public_subnet_id
 
