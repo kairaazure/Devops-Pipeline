097266341b7e9198487e6430d7034c9afec413a2
Nikhil Gupta <nikgupta5@publicisgroupe.net>
2020-02-05 23:35:46 +0530

    Changed buildspec

--- modules/InternetGateway/outputs.tf
+++ modules/InternetGateway/outputs.tf
@@ -1,5 +1,5 @@
 output "ig_id" {
   description = "route table id of cloud created"
-  value       = concat(aws_internet_gateway."${var.igname}".*.id, [""])[0]
+  value       = concat(aws_internet_gateway.var.igname.*.id, [""])[0]
 
 }
